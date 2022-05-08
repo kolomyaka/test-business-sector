717a8f09357c4c6394dacbfe8d38ab1e30ffd6d9
kolomyaka1 <ka4ok24@mail.ru>
2022-05-08 15:56:43 +0300

    fix err with deploy

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,16 +1,16 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit'
 import { useDispatch } from 'react-redux';
-import postsSliceReducer from './slices/postSlice';
+import postSliceReducer from './slices/postSlice';
 
 
 const rootReducer = combineReducers({
-  posts: postsSliceReducer
+  posts: postSliceReducer
 })
 
 export const store = configureStore({
   reducer: rootReducer,
   middleware: ((getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false }))
-}) 
+})
 
 export type RootState = ReturnType<typeof rootReducer>;
 export type AppStore = typeof store;
